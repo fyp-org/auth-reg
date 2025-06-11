a49b9ae8b9c3a15d73c9e7f0139775b31b3d2c1d
CyberGeo335 <george.dev@mail.ru>
2025-06-11 21:52:40 +0300

    fixed crud and etc

--- reg-zareg/crud.py
+++ reg-zareg/crud.py
@@ -1,23 +1,58 @@
 import time
 from sqlalchemy.orm import Session
 from models import User
+from schemas import UserCreate
 
-# in-memory кэш для пользователей
+# кэш в памяти для пользователей
+ttl = 60
 user_cache = {}
-CACHE_TTL = 60  # время жизни кэша в секундах
+
+CACHE_TTL = ttl
 
 def get_user_by_email(db: Session, email: str):
     current_time = time.time()
-    # Если пользователь уже в кэше и данные актуальны, возвращаем их
     if email in user_cache:
         cached_time, cached_user = user_cache[email]
         if current_time - cached_time < CACHE_TTL:
             return cached_user
         else:
-            # Если время кэша истекло, удаляем запись
             del user_cache[email]
-    # Если данных в кэше нет или они устарели, запрашиваем пользователя из БД
     user = db.query(User).filter(User.email == email).first()
     if user:
         user_cache[email] = (current_time, user)
+    return user
+
+def get_user_by_id(db: Session, user_id: int):
+    """
+    Retrieve a user by their ID from the database.
+    """
+    return db.query(User).filter(User.id_users == user_id).first()
+
+def create_user(db: Session, user: UserCreate):
+    """
+    Create a new user in the database based on the UserCreate schema.
+    """
+    new_user = User(
+        name=user.name,
+        second_name=user.second_name,
+        email=user.email,
+        password=user.password
+        # потом можно дополнить
+    )
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    # Кэшировать только что созданного пользователя
+    user_cache[new_user.email] = (time.time(), new_user)
+    return new_user
+
+def delete_user(db: Session, user_id: int):
+    """
+    Удалить пользователя по идентификатору из базы данных.
+    """
+    user = get_user_by_id(db, user_id)
+    if user:
+        db.delete(user)
+        db.commit()
+        user_cache.pop(user.email, None)
     return user
